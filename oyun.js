6b9ed3454eb1491d0adc656b0313eaa6d12b7043
Ozgur Kaya <okaya.gm@gmail.com>
2018-10-27 19:21:41 +0300

    deneme

--- oyun.js
+++ oyun.js
@@ -1,12 +1,14 @@
 
+/***************** Canvas *********************/
 const canvas = document.querySelector('canvas');
-canvas.width = innerWidth - 40;
-canvas.height = innerHeight -40;
+canvas.width = innerWidth-30;
+canvas.height = innerHeight-30;
 canvas.style.border = 'solid';
-
 const c = canvas.getContext('2d');
 
-const keyStatu = [false,false,false,false];
+/************ Kontroller *********************/
+let keyStatu = [false,false,false,false];
+let kosu = false;
 
 document.addEventListener('keydown',tusdown)
 function tusdown(e) {
@@ -14,6 +16,7 @@ function tusdown(e) {
         if(e.keyCode == 38) {  keyStatu[1] = true;  }
         if(e.keyCode == 39) {  keyStatu[2] = true;  }
         if(e.keyCode == 40) {  keyStatu[3] = true;  }
+        if(e.keyCode == 32) {  kosu = true;  }
 };
 
 document.addEventListener('keyup', tusup)
@@ -22,9 +25,10 @@ function tusup(e) {
         if(e.keyCode == 38) {  keyStatu[1] = false;  }
         if(e.keyCode == 39) {  keyStatu[2] = false;  }
         if(e.keyCode == 40) {  keyStatu[3] = false;  }
+        if(e.keyCode == 32) {  kosu = false;}
 };
-    
 
+/****************** Karakter *********************/    
 let Caracter = function() {
     this.adam = new Image();
     this.adam.src = 'image/avci.png';
@@ -35,21 +39,25 @@ let Caracter = function() {
     this.adamY = 0;
     this.adamW = 768;
     this.adamH = 768;
-    this.width = this.adamW / 8;
-    this.height = this.adamH / 8;
-    this.currentFrame = 0;
     this.cols = 8;
+    this.row = 8;
+    this.width = this.adamW / this.cols;
+    this.height = this.adamH / this.row;
+    this.currentFrame = 0;
     this.sayac = 0;
     this.hareket = 2;
     
     this.update = function () {
         c.drawImage(this.adam, this.adamX, this.adamY * this.height, this.width, this.height, this.x, this.y, this.width /1.2, this.height/1.2);
         
+        if(kosu) { this.hareket = 4 }
+        else { this.hareket = 2}
+
         if (keyStatu[0] && !keyStatu[1] && !keyStatu[2] && !keyStatu[3]) {
             this.x -=this.hareket;
             this.adamY = 7;
             this.draw();
-            
+           
         }
         
         if (!keyStatu[0] && keyStatu[1] && !keyStatu[2] && !keyStatu[3]) {
@@ -71,29 +79,29 @@ let Caracter = function() {
         }
 
         if (keyStatu[0] && keyStatu[1] && !keyStatu[2] && !keyStatu[3]) {
-            this.y -=this.hareket;
-            this.x -=this.hareket;
+            this.y -=this.hareket*0.7071067811865475;
+            this.x -=this.hareket*0.7071067811865475;
             this.adamY = 3;
             this.draw();
         }
 
         if (keyStatu[0] && !keyStatu[1] && !keyStatu[2] && keyStatu[3]) {
-            this.y +=this.hareket;
-            this.x -=this.hareket;
+            this.y +=this.hareket*0.7071067811865475;
+            this.x -=this.hareket*0.7071067811865475;
             this.adamY = 6;
             this.draw();
         }
 
         if (!keyStatu[0] && keyStatu[1] && keyStatu[2] && !keyStatu[3]) {
-            this.y -=this.hareket;
-            this.x +=this.hareket;
+            this.y -=this.hareket*0.7071067811865475;
+            this.x +=this.hareket*0.7071067811865475;
             this.adamY = 2;
             this.draw();
         }
 
         if (!keyStatu[0] && !keyStatu[1] && keyStatu[2] && keyStatu[3]) {
-            this.y +=this.hareket;
-            this.x +=this.hareket;
+            this.y +=this.hareket*0.7071067811865475;
+            this.x +=this.hareket*0.7071067811865475;
             this.adamY = 5;
             this.draw();
         }
@@ -114,9 +122,9 @@ let Caracter = function() {
         
     }
 }
+
+
 let icon = new Caracter();
-
-
 
 function play() {
     c.clearRect(0, 0, canvas.width, canvas.height);
